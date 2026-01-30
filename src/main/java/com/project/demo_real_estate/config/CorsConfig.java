df17eae59cf0e8bba92c01e536eac54f4369a43c
Vamsi1807 <vamsigopavarapu07@gmail.com>
2026-01-30 11:43:55 +0530

    errors resolved

---
+++ src/main/java/com/project/demo_real_estate/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.project.demo_real_estate.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "https://demo-real-estate-frontend.onrender.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+}
